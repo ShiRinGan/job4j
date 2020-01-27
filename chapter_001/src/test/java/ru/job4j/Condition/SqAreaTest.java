c56af4fc4fdd1d27112b3c45b13cd399892aaa34
ShiRinGan <shikarno1992@mail.ru>
2020-01-27 22:46:29 +0300

    Отладка. Максимум из 4-х чисел.

--- chapter_001/src/test/java/ru/job4j/Condition/SqAreaTest.java
+++ chapter_001/src/test/java/ru/job4j/Condition/SqAreaTest.java
@@ -1,13 +1,14 @@
-package ru.job4j.Сondition;
+package ru.job4j.Condition;
 
 import org.junit.Assert;
 import org.junit.Test;
+import ru.job4j.Сondition.SqArea;
 
 public class SqAreaTest {
     @Test
     public void square(){
-        int p=6;
-        int k=2;
+        int p=4;
+        int k=1;
         double expected = 2;
         double out = SqArea.square(p, k);
         Assert.assertEquals(expected, out, 0.01);
--- chapter_001/src/test/java/ru/job4j/Condition/SqMaxTest.java
+++ chapter_001/src/test/java/ru/job4j/Condition/SqMaxTest.java
@@ -1,16 +1,19 @@
 package ru.job4j.Condition;
 
+import org.junit.Assert;
 import org.junit.Test;
+import ru.job4j.Сondition.MultiMax;
 import ru.job4j.Сondition.SqMax;
 
 import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
 public class SqMaxTest {
     @Test
-    public void whenForthMax() {
+    public void foundMax() {
         SqMax check = new SqMax();
-        int result = check.max(1, 4, 2, 5);
-        assertThat(result, is(5));
+        int result = check.max(1000, 100, 10, 1);
+        assertThat(result, is(1000));
     }
 }
