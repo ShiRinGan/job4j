c5d6ad36ab15b5616166dbff56d84a109fa2e0db
ShiRinGan <shikarno1992@mail.ru>
2020-01-30 22:50:40 +0300

    Треугольник #Fix

--- chapter_001/src/main/java/ru/job4j/Сondition/Triangle.java
+++ chapter_001/src/main/java/ru/job4j/Сondition/Triangle.java
@@ -1,16 +1,14 @@
 package ru.job4j.Сondition;
 
 public class Triangle {
-    public static boolean exist(double ab, double ac, double bc){
-        boolean op1 = (ab+ac)>bc;
-        boolean op2 = (ac+bc)>ab;
-        boolean op3 = (ab+bc)>ac;
-        boolean result = true;
-                if (op1&&op2&&op3) {
-                    return true;}
-                    else {
-                    return (op1&&op2&&op3);
-                }
+    public static boolean exist(double ab, double ac, double bc) {
+        boolean op1 = (ab + ac) > bc;
+        boolean op2 = (ac + bc) > ab;
+        boolean op3 = (ab + bc) > ac;
+        if (op1 && op2 && op3) {
+            return true;
+        } else {
+            return false;
         }
     }
-
+}
