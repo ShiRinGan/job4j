0737b596f900e760f9d48235df24cc338448f0a5
ShiRinGan <shikarno1992@mail.ru>
2020-02-01 00:37:49 +0300

    Подчсет суммы чётных чисел в диапазоне

---
+++ chapter_001/src/test/java/loop/CounterTest.java
@@ -0,0 +1,23 @@
+package loop;
+
+import org.junit.Test;
+import ru.job4j.loop.Counter;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class CounterTest {
+
+    @Test
+    public void whenSumEvenNumbersFromOneToTenThenThirty() {
+        int rsl = Counter.sumByEven(1, 10);
+        int expected = 30;
+        assertThat(rsl, is(expected));
+    }
+    @Test
+    public void sumEachOver() {
+        int rsl = Counter.sum(1, 10);
+        int expected = 55;
+        assertThat(rsl, is(expected));
+    }
+}
