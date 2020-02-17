d98d956fc573b0d6b1c30dab2b5b07fb416bc359
ShiRinGan <shikarno1992@mail.ru>
2020-02-17 19:55:35 +0300

    простые числа fix

--- chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -5,14 +5,12 @@ public class CheckPrimeNumber {
 int num = 0;
         boolean prime = finish >1;
         for (int index = 2; index < finish; index++) {
-num+=index;
+
             if ((finish%index == 0)) {
                 prime = false;
                 break;
             }
-            else if(finish>=num){
-                    break;
-                }
+
             }
             return prime;
         }}
--- chapter_001/src/test/java/loop/CheckPrimeNumberTest.java
+++ chapter_001/src/test/java/loop/CheckPrimeNumberTest.java
@@ -9,19 +9,19 @@ import static org.junit.Assert.*;
 public class CheckPrimeNumberTest {
     @Test
     public void when5() {
-        boolean rsl = CheckPrimeNumber.check(2);
+        boolean rsl = CheckPrimeNumber.check(11);
         assertThat(rsl, is(true));
     }
 
     @Test
     public void when4() {
-        boolean rsl = CheckPrimeNumber.check(4);
+        boolean rsl = CheckPrimeNumber.check(12);
         assertThat(rsl, is(false));
     }
 
     @Test
     public void when1() {
-        boolean rsl = CheckPrimeNumber.check(1);
+        boolean rsl = CheckPrimeNumber.check(14);
         assertThat(rsl, is(false));
     }
 }
