6706cd925d03d7323e7da9d9628d70db26ac8840
ShiRinGan <shikarno1992@mail.ru>
2020-02-19 20:21:21 +0300

    JavaDoc 2.0

--- chapter_001/src/main/java/ru/job4j/calculator/calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/calculator.java
@@ -8,22 +8,54 @@ package ru.job4j.calculator;
  *
  */
 public class calculator {
+    /**
+     * Сложение
+     * @param first
+     * @param second
+     * @return результат
+     */
     public static void add(double first, double second) {
         double result = first + second;
         System.out.println(first + " + " + second + " = " + result);
     }
+
+    /**
+     * Деление
+     * @param first
+     * @param second
+     * @return результат
+     */
     public static void div(double first, double second) {
         double result = first / second;
         System.out.println(first + " / " + second + " = " + result);
     }
+
+    /**
+     * Умножение
+     * @param first
+     * @param second
+     * @return вывод на экран результата
+     */
     public static void multiply(double first, double second) {
         double result = first * second;
         System.out.println(first + " * " + second + " = " + result);
     }
+
+    /**
+     * Вычитание
+     * @param first
+     * @param second
+     * @return результат
+     */
     public static void subtrack(double first, double second) {
         double result = first - second;
         System.out.println(first + " - " + second + " = " + result);
     }
+
+    /**
+     * Объявление значения переменным
+     * @param args
+     */
     public static void main(String[] args) {
         add(1, 1);
         div(4, 2);
