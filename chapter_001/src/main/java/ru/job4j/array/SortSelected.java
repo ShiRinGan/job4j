e597da68bac2b7230dbfbc6e403eb4793b5714af
ShiRinGan <shikarno1992@mail.ru>
2020-04-22 23:35:26 +0300

    Сотрировка выбором (work)

--- chapter_001/src/main/java/ru/job4j/array/SortSelected.java
+++ chapter_001/src/main/java/ru/job4j/array/SortSelected.java
@@ -2,18 +2,20 @@ package ru.job4j.array;
 
 public class SortSelected {
     public static int[] sort(int[] data) {
-        for (int i=0; i<data.length; i++) {
-            int min = MinDiapason.findMin(data, 0, data.length);
-            int index = FindLoop.indexOf(data, min, 0, data.length);
-            if (index<min){
-                min=index;
-                for (index=0; index<data.length;index++){
-                    int temp=data[index];
-                    data[index]=data[min];
-                    data[min]=temp;
-                }
-            }
+        for (int i=0; i<data.length; i++) { // начинаем работы с создания цикла, с которым будем работать.
+            int min = MinDiapason.findMin(data, i, data.length-1); // первым шагом будет поиск минимального элемента в текущем списке. data.length-1!!! главное -1 иначе ошибка
+            // int min = data[start]; тут стартом будет являтся переменная i
+            //        for (int index = start ; index <= finish; index++) { тут финиш это data.length-1!!! главное -1 иначе ошибка
+            //            if (data[index]<min){
+            //            min = data[index];
+            int index = FindLoop.indexOf(data, min, i, data.length); // поиск элемента в массиве
+            // for (int index = start; index < finish; index++){ цикл
+            //            if (data[index] == el){ элементов в даннои цикле будет являтся min, которое мы нашли прошлом методе
+            //                rst = index; результат
+            //                break;
+            int temp=data[i]; // создаём переменную для замены.
+            data[i]=data[index]; // сопоставляем элементы
+            data[index]=temp; // меняем местами
         }
         return data;
-}
-}
+}}
--- chapter_001/src/test/java/array/SortSelectedTest.java
+++ chapter_001/src/test/java/array/SortSelectedTest.java
@@ -14,4 +14,10 @@ public class SortSelectedTest {
         int[] expect = new int[] {1, 2, 3, 4, 5};
         assertThat(result, is(expect));
     }
+    @Test
+    public void whenSort() {
+        int[] input = new int[] {3, 1, 2};
+        int[] result = SortSelected.sort(input);
+        int[] expect = new int[] {1, 2, 3};
+        assertThat(result, is(expect));
 }
