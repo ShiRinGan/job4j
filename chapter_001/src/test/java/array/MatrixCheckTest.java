177b08a21b435c08700d8321375d8ddabdbc7869
ShiRinGan <shikarno1992@mail.ru>
2020-04-23 20:00:57 +0300

    Моно строка в матице

---
+++ chapter_001/src/test/java/array/MatrixCheckTest.java
@@ -0,0 +1,21 @@
+package array;
+
+import org.junit.Test;
+import ru.job4j.array.MatrixCheck;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class MatrixCheckTest {
+
+    @Test
+    public void whenHasMonoHorizontal() {
+        char[][] input = {
+                {' ', ' ', ' '},
+                {'X', 'X', 'X'},
+                {' ', ' ', ' '},
+        };
+        boolean result = MatrixCheck.monoHorizontal(input, 1);
+        assertThat(result, is(true));
+    }
+}
