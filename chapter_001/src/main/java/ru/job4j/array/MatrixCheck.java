77456c49ee40c53a4a295cdc994d3ee0d8e47873
ShiRinGan <shikarno1992@mail.ru>
2020-04-24 20:39:48 +0300

    Моно столбец и моно строка в матрице с одним циклом fix

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -4,22 +4,20 @@ public class MatrixCheck {
     public static boolean monoHorizontal(char[][] board, int row) {
             boolean result = true;
             for (int i=0; i<board.length-1; i++) {
-                for (int index=0; index<board.length-1; index++){
-                if (board[row][i] != board[row][i+index]) {
+                if (board[row][i] != 'X') {
                     result = false;
                     break;
-                }}
+                }
             }
             return result;
         }
     public static boolean monoVertical(char[][] board, int column) {
         boolean result = true;
         for (int j=0; j<board.length-2; j++) {
-            for (int index=0; index<board.length-2; index++){
-            if (board[j][column] != board[j][column+index]) {
+            if (board[j][column] != 'X') {
                 result = false;
                 break;
-            }}
+            }
         }
         return result;
     }
