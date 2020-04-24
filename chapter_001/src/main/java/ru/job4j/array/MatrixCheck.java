e400fd1106221261b9fd7a0798766519898a72ab
ShiRinGan <shikarno1992@mail.ru>
2020-04-24 23:54:23 +0300

    Выигрышные комбинации в сокобан

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -2,18 +2,19 @@ package ru.job4j.array;
 
 public class MatrixCheck {
     public static boolean monoHorizontal(char[][] board, int row) {
-            boolean result = true;
-            for (int i=0; i<board.length; i++) {
-                if (board[row][i] != 'X') {
-                    result = false;
-                    break;
-                }
+        boolean result = true;
+        for (int i = 0; i < board.length; i++) {
+            if (board[row][i] != 'X') {
+                result = false;
+                break;
             }
-            return result;
         }
+        return result;
+    }
+
     public static boolean monoVertical(char[][] board, int column) {
         boolean result = true;
-        for (int j=0; j<board.length; j++) {
+        for (int j = 0; j < board.length; j++) {
             if (board[j][column] != 'X') {
                 result = false;
                 break;
@@ -21,12 +22,25 @@ public class MatrixCheck {
         }
         return result;
     }
+
     public static char[] extractDiagonal(char[][] board) {
         char[] rsl = new char[board.length];
-        for ( int index=0; index<board.length; index++) {
-            if (board[index][index] == 'X'){
+        for (int index = 0; index < board.length; index++) {
+            if (board[index][index] == 'X') {
                 rsl[index] = board[index][index];
             }
-            }
+        }
         return rsl;
-    }}
+    }
+
+    public static boolean isWin(char[][] board) {
+        boolean result = false;
+        for (int index = 0; index < board.length; index++) {
+            if (monoHorizontal(board, index) || monoVertical(board, index)) {
+                result = true;
+                break;
+            }
+        }
+        return result;
+    }
+}
