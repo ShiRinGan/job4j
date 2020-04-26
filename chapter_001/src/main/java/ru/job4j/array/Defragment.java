2ab301bed206519d5f777829647bec74538b0912
ShiRinGan <shikarno1992@mail.ru>
2020-04-26 16:33:55 +0300

    Дефрогментация (не понятно)

---
+++ chapter_001/src/main/java/ru/job4j/array/Defragment.java
@@ -0,0 +1,28 @@
+package ru.job4j.array;
+
+public class Defragment {
+    public static String[] compress(String[] array) {
+        for (int index = 0; index < array.length; index++) {
+            if (array[index] == null) {
+                int point = index; // указатель, на не null ячейку.
+                index=array[index];// переместить первую не null ячейку
+                array[index]=point;
+                while (index<=array.length);{// Здесь нужен цикл while
+                   // ???
+                }
+
+            }
+            System.out.print(array[index] + " ");
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        String[] input = {"I", null, "wanna", null, "be", null, "compressed"};
+        String[] compressed = compress(input);
+        System.out.println();
+        for (int index = 0; index < compressed.length; index++) {
+            System.out.print(compressed[index] + " ");
+        }
+    }
+}
