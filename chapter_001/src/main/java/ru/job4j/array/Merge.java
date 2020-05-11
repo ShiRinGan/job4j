b1eb2c06ca25d6122ecd85a67f4a86e6cd404bf3
ShiRinGan <shikarno1992@mail.ru>
2020-05-11 20:33:57 +0300

    Объединение двух массивов

--- chapter_001/src/main/java/ru/job4j/array/Merge.java
+++ chapter_001/src/main/java/ru/job4j/array/Merge.java
@@ -1,17 +1,22 @@
 package ru.job4j.array;
+
 import java.util.Arrays;
 
-public class Merge {
+public class Merge { //объединение двух массивов без приминения сортировки
 
-        public int[] merge(int[] left, int[] right) {
+    public int[] merge(int[] left, int[] right) {
         int[] rsl = new int[left.length + right.length];
-        int count=0;
-        for (int i=0; i<left.length; i++){
-            rsl[i]=left[i];
-            count++;
-        }
-        for (int j=0; j<right.length; j++){
-            rsl[count++]=right[j];
+        int iLeft = 0; //задать переменную для левой части
+        int iRight = 0; //задать переменную для правой части
+        int iResult = 0; //задать переменную для результатирующего массива
+        while (iLeft + iRight != rsl.length) { //пока сумма двух массивов не равна
+            if (iLeft != left.length && iRight != right.length) { //проверяем первое условие
+                rsl[iResult++] = left[iLeft] < right[iRight] ? left[iLeft++] : right[iRight++];
+            } else if (iLeft != left.length) {
+                rsl[iResult++] = left[iLeft++];
+            } else {
+                rsl[iResult++] = right[iRight++];
+            }
         }
         return rsl;
     }
@@ -19,8 +24,8 @@ public class Merge {
     public static void main(String[] args) {
         Merge process = new Merge();
         int[] rsl = process.merge(
-                new int[] {1, 3, 5},
-                new int[] {2, 4}
+                new int[]{1, 3, 5},
+                new int[]{2, 4}
         );
         System.out.println(Arrays.toString(rsl));
     }
