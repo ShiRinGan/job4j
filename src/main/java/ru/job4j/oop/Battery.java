036d2a7bb0cc0bc2c8912756c59bf7eea15f9bf8
ShiRinGan <shikarno1992@mail.ru>
2020-05-26 22:29:12 +0300

    Состояние объекта

--- src/main/java/ru/job4j/oop/Battery.java
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -8,15 +8,15 @@ public class Battery {
     }
 
     public void exchange(Battery another) {
-        this.load = this.load + another.load;
-        another.load = 0;
+        another.load = another.load + this.load;
+        this.load = 0;
     }
 
     public static void main(String[] args) {
-        Battery first = new Battery(5);
-        Battery second = new Battery(10);
+        Battery first = new Battery(10);
+        Battery second = new Battery(5);
         System.out.println("First: " + first.load + ". Second: " + second.load);
-        second.exchange(first);
+        first.exchange(second);
         System.out.println("First: " + first.load + ". Second: " + second.load);
     }
 }
