caceabf14ddbb0df39b92688abf4e0043a84c8c6
ShiRinGan <shikarno1992@mail.ru>
2020-06-13 18:47:46 +0300

    Перегрузка конструктора

--- chapter_002/src/main/java/ru/job4j/tracker/Item.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Item.java
@@ -4,12 +4,16 @@ public class Item {
     public Item() {
     }
 
+    public Item(String name) {
+    }
+
     public Item(String name, String id) {
     }
 
+
     public static void main(String[] args) {
         Item item = new Item();
-        Item onlyName = new Item();
-        Item onlyId = new Item();
+        Item onlyName = new Item("Filipp");
+        Item nameAndId = new Item("Filipp", 28);
     }
 }
