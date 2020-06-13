b14e4c2f3121a8ac5e11c19de29e9863fef88b74
ShiRinGan <shikarno1992@mail.ru>
2020-06-13 17:39:24 +0300

    Перегрузка конструктора
    Перегрузка конструктора
    
    Перегрузка конструктора
    
    Перегрузка конструктора

---
+++ chapter_002/src/main/java/ru/job4j/tracker/Item.java
@@ -0,0 +1,19 @@
+package ru.job4j.tracker;
+
+public class Item {
+    public Item() {
+    }
+
+    public Item(String name) {
+    }
+
+    public Item(String name, String id) {
+    }
+
+
+    public static void main(String[] args) {
+        Item item = new Item();
+        Item onlyName = new Item("Filipp");
+        Item nameAndId = new Item("Filipp", "28");
+    }
+}
