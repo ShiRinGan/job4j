ae92241fc13117e5487ff5183a91ef2f2654fc7f
ShiRinGan <shikarno1992@mail.ru>
2020-06-13 17:39:24 +0300

    Перегрузка конструктора

---
+++ chapter_002/src/main/java/ru/job4j/tracker/Item.java
@@ -0,0 +1,13 @@
+package ru.job4j.tracker;
+
+public class Item {
+    public Item() {
+    }
+
+    public Item(String name, String id) {
+    }
+
+    public static void main(String[] args) {
+        Item item = new Item();
+    }
+}
