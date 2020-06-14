8c7737947421250222a51f6d94d5102b60d85c20
ShiRinGan <shikarno1992@mail.ru>
2020-06-14 21:20:51 +0300

    Переопределение

--- chapter_002/src/main/java/ru/job4j/inheritance/JSONReport.java
+++ chapter_002/src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -2,7 +2,7 @@ package ru.job4j.inheritance;
 
 public class JSONReport extends TextReport {
     public String generate(String name, String body) {
-        return "name: " + "name" +
-                " body: " + "body";
+        return "name" : "name",
+                "body" : "body"
     }
 }
