2df42da7d4a555e89088e1fe85620eae7e78e32d
ShiRinGan <shikarno1992@mail.ru>
2020-06-14 19:36:12 +0300

    Реализация профессий в коде

--- chapter_002/src/main/java/ru/job4j/tracker/Profession.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Profession.java
@@ -7,7 +7,7 @@ public class Profession {
     private String birthday;
 
     public String getName() {
-        return name;
+        return this.name;
     }
 
     public String getSurname() {
@@ -24,17 +24,59 @@ public class Profession {
 
     public class Engineer extends Profession {
         public class Programmer extends Engineer {
+            private String language;
+            private String level;
+
+            public String getLanguage() {
+                return language;
+            }
+
+            public String getLevel() {
+                return level;
+            }
         }
 
         public class Builder extends Engineer {
+            private String desing;
+            private String experience;
+
+            public String getDesing() {
+                return desing;
+            }
+
+            public String getExperience() {
+                return experience;
+            }
         }
     }
 
     public class Doctor extends Profession {
         public class Surgeon extends Doctor {
+            private String pain;
+            private String organ;
+
+            public String getPain() {
+                return pain;
+            }
+
+            public String getOrgan() {
+                return organ;
+            }
+
         }
 
         public class Dentist extends Doctor {
+            private String teeth;
+            private String drug;
+
+            public String getTeeth() {
+                return teeth;
+            }
+
+            public String getDrug() {
+                return drug;
+            }
+
         }
     }
 }
